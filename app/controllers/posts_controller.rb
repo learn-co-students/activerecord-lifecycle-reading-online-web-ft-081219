90c83f5c24d8688a4727ffc0ff34f42a98c7c05c
Armel <armelfli50@gmail.com>
2019-10-21 00:38:47 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,9 +21,14 @@ class PostsController < ApplicationController
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-	  @post.update(params.require(:post).permit(:title, :description, :post_status, :author_id))
-	  redirect_to post_path(@post)
+		@post = Post.find(params[:id])
+		
+		@post.update(params.require(:post).permit(:title, :description, :post_status, :author_id))
+		if @post.valid? 
+			redirect_to post_path(@post)
+		else
+			render "posts/edit"
+		end
 	end
 
 	def edit
